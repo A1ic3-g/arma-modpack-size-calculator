2053acb79a13eb660ce4b1ffdebf4129d5ecf40d
Alice Gowler <alicegowler@outlook.com>
2022-08-19 23:34:04 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# arma-modpack-size-calculator
+
+Simple script to calculate the size an Arma 3 modpack takes up on disk
+
+## Usage
+
+1. Install requirements from requirments.txt using pip
+    ```pip install -r requirements.txt```
+2. Run the script using `python3` (use `python` on windows)
+    ```python3 modpack-size-calculator.py```
+
+### Caveats
+
+The workshop path required is the one in the `SteamLibrary\workshop\content\107410` folder NOT the !WORKSHOP folder in the arma installation folder.
---
+++ modpack-size-calculator.py
@@ -0,0 +1,72 @@
+import os
+from bs4 import BeautifulSoup
+from hurry.filesize import filesize
+
+ID_START = '?id='
+
+# Getsize courtesy of monkut on stackoverflow
+# https://stackoverflow.com/questions/1392413/calculating-a-directorys-size-using-python
+def get_size(start_path = '.'):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            print(fp)
+            # skip if it is symbolic link
+            if not os.path.islink(fp):
+                total_size += os.path.getsize(fp)
+
+    return total_size
+
+
+if __name__ == "__main__":
+
+    modpack_found = False
+    while modpack_found == False: 
+        modpack_path = str(input("Enter path to modpack .html file: "))
+        if os.path.exists(modpack_path):
+            modpack_found = True 
+        else:
+            print("File not found, please try again")
+    
+    with open(modpack_path, 'r') as f:
+        modpack_string =  f.read()
+
+    html_soup = BeautifulSoup(modpack_string, 'html.parser')
+    
+    # Get mod IDs of all mods in the modpack
+    modpack_ids = []
+    for link in html_soup.find_all('a'):
+        page = link.get('href')
+        id_loc = page.find(ID_START)
+        id = page[id_loc+len(ID_START):]
+        modpack_ids.append(id)
+
+    # Get workshop folder containing mods
+    # Typically steamapps/workshop/content/107410
+    # Note: this is NOT the !WORKSHOP folder 
+    workshop_folder_found = False
+    while workshop_folder_found == False:
+        workshop_folder_path = str(input("Enter workshop folder path: "))
+
+        if os.path.exists(workshop_folder_path) and os.path.dirname != "!WORKSHOP":
+            workshop_folder_found = True 
+        else:
+            print("Invalid Folder path entered")
+            print("please ensure you're NOT entering your !WORKSHOP folder path")
+            print("A typical true workshop folder path looks like steamapps\workshop\content\\107410")
+
+    # Get all the folder sizes
+    total_size = 0
+    mod_sizes = []
+    for modpack_id in modpack_ids:
+        mod_path = os.path.join(workshop_folder_path, modpack_id)
+        print(mod_path)
+        size = get_size(start_path=mod_path)
+        print(size)
+        total_size += size
+        mod_sizes.append(size)
+
+
+    print(f"Traditional 1024 scale (used by windows): {filesize.size(total_size)}")
+    print(f"SI 1000 scale: {filesize.size(total_size, system=filesize.si)}")
